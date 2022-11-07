4a246958ea79ed6bc44d2921acf5eb08479c3bf0
torngren1337 <117650641+torngren1337@users.noreply.github.com>
2022-11-07 14:23:48 +0100

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,10 @@
 
-public class HelloWorld{
-    public static void main (String [] args){
+public class HelloWorld {
+    public static void main(String[] args) {
 
         System.out.println("Goodbye World!");
-}
+        System.out.println("jfiejfiefjie");
+
+    }
 
 }
