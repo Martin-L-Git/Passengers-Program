7e0a10f3f133df199f387359e3c62bb36cf54474
Martin Lidgren <martin.lidgren@live.se>
2022-11-07 14:38:52 +0100

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,9 +2,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
 
-        System.out.println("Goodbye World!");
-        System.out.println("jfiejfiefjie");
-        System.out.println("Nånting");
+        System.out.println("Hello World");
     
 
     }
