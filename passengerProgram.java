0d665b7bfd6c81b424b02da2865c0206c2ec24a2
Martin Lidgren <martin.lidgren@live.se>
2022-11-15 11:57:49 +0100

    Update passengerProgram.java

--- passengerProgram.java
+++ passengerProgram.java
@@ -8,11 +8,11 @@ public class passengerProgram {
         Passenger[] passengers = Passenger.getPassengerList();
         System.out.println("Write the number in which order you'd like to proceed");
         System.out.println(" 1. Add Passenger \n 2. Remove Passenger \n 3. Switch Seats \n 4. Rename Passenger \n 5. Count Passengers \n 6. Print Passenger Manifest");
-        menu_choice(passengers);
+        menu(passengers);
         }
 
 
-    public static void menu_choice(Passenger[] passengers){
+    public static void menu(Passenger[] passengers){
         int choice = myScanner.nextInt();
         
         // Creates a new passenger and puts them in the first available seat
